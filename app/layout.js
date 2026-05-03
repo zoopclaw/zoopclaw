95adfabab864f95ffbe3897a0c4de15bcadde960
zoopclaw <zoopclaw@gmail.com>
2026-05-03 16:09:48 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "ZoopClaw",
+  description: "Execução na velocidade de Planck"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body style={{
+        margin: 0,
+        padding: 0,
+        backgroundColor: "#000"
+      }}>
+        {children}
+      </body>
+    </html>
+  )
+}
